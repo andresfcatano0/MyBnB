749a7b149c78cce1a8d4a1b25150b0beaa17161d
Andres Catano <andresfcatano0@gmail.com>
2025-03-23 16:48:55 -0400

    Created campground delete middleware to delete associated reviews

--- models/accommodation.js
+++ models/accommodation.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const AccommodationSchema = new Schema({
@@ -14,5 +15,18 @@ const AccommodationSchema = new Schema({
     }
   ]
 });
+
+// (Query middleware) findOneAndDelete only triggered by findByIdAndDelete
+// Can still access what was just deleted
+AccommodationSchema.post('findOneAndDelete', async function(doc) {
+  if(doc) {
+    await Review.deleteMany({ 
+      // id for each review in deleted campground's reviews array
+      _id: {
+        $in: doc.reviews
+      }
+    })
+  }
+})
 
 module.exports = mongoose.model('Accommodation', AccommodationSchema);
