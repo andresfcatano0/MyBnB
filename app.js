e10759260ac0bad26846b2eec339e20372445e1e
Andres Catano <andresfcatano0@gmail.com>
2025-03-31 11:49:50 -0400

    Refactored app.js to move express Router logic to separate files

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const { accommodationSchema, reviewSchema } = require("./joiSchemas.js");
 
 const accommodations = require("./routes/accommodations");
+const reviews = require("./routes/reviews");
 
 const Accommodation = require("./models/accommodation");
 const Review = require("./models/review");
@@ -29,44 +28,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400 )
-  } else {
-    next();
-  }
-}
-
 app.use('/accommodations', accommodations);
+app.use('/accommodations/:id/reviews', reviews);
 
 app.get('/', (req, res) => {
   res.render('home');
 })
-
-// Creates new review on server
-app.post('/accommodations/:id/reviews', validateReview, catchAsync(async (req, res) => {
-  const accommodation = await Accommodation.findById(req.params.id);
-  const review = new Review(req.body.review);
-  // console.log("ACCOMMODATION:" + accommodation)
-  // console.log("REVIEW:" + review)
-  // console.log("REQ.PARAMS " + JSON.stringify(req.params, null, 2));
-  // console.log("REQ.BOODY " + JSON.stringify(req.body, null, 2));
-  // Push onto accommodation model reviews 
-  accommodation.reviews.push(review);
-  await review.save();
-  await accommodation.save();
-  res.redirect(`/accommodations/${accommodation._id}`);
-}))
-
-app.delete('/accommodations/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-  const { id, reviewId } = req.params;
-  // $pull - mongo operator to remove reference to specific review from an accommodation
-  await Accommodation.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/accommodations/${id}`);
-}))
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
