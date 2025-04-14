4c5cc4214172c82dfcaac92f26674f9b5efcf948
Andres Catano <andresfcatano0@gmail.com>
2025-04-14 11:48:31 -0400

    Added logout functionality

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,14 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
   res.redirect('/accommodations');
 });
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+      if (err) {
+          return next(err);
+      }
+      req.flash('success', 'Goodbye!');
+      res.redirect('/accommodations');
+  });
+}); 
+
 module.exports = router;
