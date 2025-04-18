8a823e7e53fceb0d3d01c3f34547e87f1cfa4093
Andres Catano <andresfcatano0@gmail.com>
2025-04-17 20:20:18 -0400

    Added author to seeds db file

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const accommodation = new Accommodation({
+      author: '6801996264463400ea520cbf',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: `https://picsum.photos/400?random=${Math.random()}`,
